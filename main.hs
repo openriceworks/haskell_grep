0f60419a454def3b8707411610e7a2708c998b07
PROBOOK-KAIMAI\sst <kaimai@sstinc.co.jp>
2018-08-23 18:40:54 +0900

    add main.hs

---
+++ main.hs
@@ -0,0 +1,55 @@
+import System.Environment (getArgs)
+import System.IO
+import Control.Exception
+
+parseArgs :: [[Char]] -> ([Char],Maybe [Char])
+parseArgs (pattern : file : _ ) = (pattern , Just(file))
+parseArgs (pattern : _) = (pattern , Nothing)
+
+contain :: [Char] -> [Char] -> Bool
+contain pattern text | length pattern > length text = False
+                      | pattern == (take (length pattern) text) = True
+                      | pattern /= (take (length pattern) text) = (contain pattern (tail text))
+
+
+matchFile :: [Char] -> [Char] -> IO [[Char]]
+matchFile pattern file = do
+  string <- readFile file
+  let strings = lines string
+  let result = filter (\x -> contain pattern x) strings
+  return result
+
+stdinMode :: [Char] -> IO()
+stdinMode pattern =
+  getLine >>= (\s ->
+    if null s then return ()
+              else putStr (if s == pattern then s else  "")  >> evaluate pattern >>= stdinMode)
+
+stdinDo :: [Char] -> IO()
+stdinDo pattern = do
+  line <- getLine
+  eof <- isEOF
+  if eof --null line ||
+    then return ()
+    else if contain pattern line
+        then evaluate pattern >>=
+        (\x -> do
+          putStrLn x
+          return x >>= stdinDo )
+        else evaluate pattern >>= stdinDo
+
+
+printList :: [[Char]] -> IO()
+printList [] = return ()
+printList (x:xs) = putStrLn x >> printList xs
+
+modeChange :: [Char] -> Maybe[Char] -> IO ()
+modeChange pattern (Just file) = matchFile pattern file >>= printList
+modeChange pattern Nothing = evaluate pattern >>= stdinDo
+
+
+main :: IO()
+main = do
+  args <- getArgs
+  let (pattern,file) = parseArgs args
+  modeChange pattern file
